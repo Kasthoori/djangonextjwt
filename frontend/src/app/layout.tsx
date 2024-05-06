74af8b02d2d7e3d4cb7a5c6c49655626553ffd77
Kasthoori <ap.kasth@gmail.com>
2024-05-06 23:00:59 +1200

    add bootstrap to frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
+import head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,17 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+            crossOrigin="anonymous"
+          />
+          <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+            crossOrigin="anonymous" 
+          />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
